d88c52868ee5d8864e788a5c255a7868cdbef48a
Rodrigo Elias <elias.rodrigo@estudante.ufjf.br>
2021-09-12 08:39:28 -0300

    Adiciona lista de filmes

--- app/src/main/java/com/example/atividade6/MainActivity.java
+++ app/src/main/java/com/example/atividade6/MainActivity.java
@@ -5,13 +5,27 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerViewFilmes;
+    List<Filmes> filmes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        filmes = new ArrayList<Filmes>(){{
+            add(new Filmes("Matrix", "Ficção Científica", "Lana Wachowski, Lilly Wachowski", 1999));
+            add(new Filmes("2012", "Ficção Científica", "Roland Emmerich", 2009));
+            add(new Filmes("O Silêncio dos Inocentes", "Terror/Suspense", "Jonathan Demme", 1991));
+            add(new Filmes("A Máscara do Zorro", "Ação/Aventura", "Martin Campbell", 1998));
+            add(new Filmes("Jolt", "Ação/Comédia", "Tanya Wexler", 2021));
+            add(new Filmes("Donnie Darko", "Ficção Científica/Fantasia", " Richard Kelly", 2001));
+        }
+
+        };
 
         recyclerViewFilmes = findViewById(R.id.recyclerViewFilmes);
     }
