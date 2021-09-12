d5190e2f25552890dd61831515c307ff40019b06
Rodrigo Elias <elias.rodrigo@estudante.ufjf.br>
2021-09-12 08:21:53 -0300

    Cria classe filmes

---
+++ app/src/main/java/com/example/atividade6/Filmes.java
@@ -0,0 +1,47 @@
+package com.example.atividade6;
+
+public class Filmes {
+    private String titulo;
+    private String estilo;
+    private String diretor;
+    private Integer ano;
+
+    public Filmes(String titulo, String estilo, String diretor, Integer ano) {
+        this.titulo = titulo;
+        this.estilo = estilo;
+        this.diretor = diretor;
+        this.ano = ano;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getEstilo() {
+        return estilo;
+    }
+
+    public void setEstilo(String estilo) {
+        this.estilo = estilo;
+    }
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+}
--- app/src/main/java/com/example/atividade6/MainActivity.java
+++ app/src/main/java/com/example/atividade6/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.atividade6;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerViewFilmes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewFilmes = findViewById(R.id.recyclerViewFilmes);
     }
 }
